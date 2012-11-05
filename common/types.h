91c40927822b63810376dd25c406489db4cf44c9
Bruna Moreira <bruna.moreira@openbossa.org>
2012-11-05 11:37:33 -0400

    ShoppingMall: Add new defines

--- common/types.h
+++ common/types.h
@@ -5,4 +5,10 @@
 #define COMPANY_ID 0xffee
 #define WALLCLOCK_OBSERVER_PATH "/test/wallclock"
 
+/* Defines for Shopping Mall demo */
+#define MALL_COMPANY_ID 0xffaa
+#define GEN_INFO 0x0
+#define SPECIAL_OFFERS 0x01
+#define FOOD_COURT 0x02
+
 #endif // _TYPES_H_
