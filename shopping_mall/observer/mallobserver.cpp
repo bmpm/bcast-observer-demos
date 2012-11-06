54ab9882c3054d784f5b773edbce6151f2c84cc2
Bruna Moreira <bruna.moreira@openbossa.org>
2012-11-06 08:52:42 -0400

    ShoppingMall: Add RegisterManufacturerObserver() function
    Implement D-Bus calls for getting default adapter and register a
    manufacturer obserser for food court.

--- shopping_mall/observer/mallobserver.cpp
+++ shopping_mall/observer/mallobserver.cpp
@@ -1,10 +1,21 @@
+#include "../../common/adapter.h"
+#include "../../common/manager.h"
 #include "../../common/types.h"
 
 #include "mallobserver.h"
 
+using namespace org::bluez;
+
 MallObserver::MallObserver(QObject *parent) :
     ObserverAdaptor(parent)
 {
+    Manager manager(BLUEZ_SERVICE_NAME, "/", QDBusConnection::systemBus());
+    foreach (const QDBusObjectPath path, manager.adapters()) {
+        Adapter adp(BLUEZ_SERVICE_NAME, path.path(),
+                                  QDBusConnection::systemBus());
+        adp.RegisterManufacturerObserver(QDBusObjectPath(MALL_OBSERVER_PATH),
+                                         MALL_COMPANY_ID);
+    }
 }
 
 void MallObserver::ManufacturerReceived(const QString &address, ushort cid,
