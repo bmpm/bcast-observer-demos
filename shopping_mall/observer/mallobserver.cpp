dac1c1b93ae495096d04239cb955598fcb0adc05
Bruna Moreira <bruna.moreira@openbossa.org>
2012-11-06 08:38:26 -0400

    ShoppingMall: Add skeleton for ManufacturerReceived()
    The method ManufacturerReceived() was added in MallObserver class. The
    property order was added too.

--- shopping_mall/observer/mallobserver.cpp
+++ shopping_mall/observer/mallobserver.cpp
@@ -1,6 +1,28 @@
+#include "../../common/types.h"
+
 #include "mallobserver.h"
 
 MallObserver::MallObserver(QObject *parent) :
-    QObject(parent)
+    ObserverAdaptor(parent)
 {
+}
+
+void MallObserver::ManufacturerReceived(const QString &address, ushort cid,
+                                             const QByteArray &value)
+{
+    qDebug() << address << cid << value.toHex();
+
+    switch (value[0]) {
+    case GEN_INFO:
+        qDebug() << "General Information";
+        break;
+    case SPECIAL_OFFERS:
+        qDebug() << "Special Offers";
+        break;
+    case FOOD_COURT:
+        qDebug() << "Food Court";
+        break;
+    default:
+        qDebug() << "Invalid value";
+    }
 }
